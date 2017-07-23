e4ff6104be511d04ead79cfb65f0d3255bd0cc0b
Hackdom <hackdom@users.noreply.github.com>
2017-07-23 14:46:35 -0500

    Add travis

--- MintedTokenAuction/truffle/migrations/2_test_contracts.js
+++ MintedTokenAuction/truffle/migrations/2_test_contracts.js
@@ -11,40 +11,8 @@ module.exports = function(deployer, network) {
   deployer.deploy(ArrayUtilsLib,{overwrite: false});
   deployer.link(BasicMathLib, ERC20Lib);
   deployer.deploy(ERC20Lib,{overwrite: false});
-  if(network !== "live"){
-    deployer.link(BasicMathLib, MintedTokenAuctionTest);
-    deployer.link(ArrayUtilsLib, MintedTokenAuctionTest);
-    deployer.link(ERC20Lib, MintedTokenAuctionTest);
-    deployer.link(ERC20Lib, StandardToken);
-    if(network == "rinkeby") {
-      var d = new Date().valueOf();
-      d = (d-5000)/1000;
-      deployer.deploy(StandardToken).then(function() {
-        return deployer.deploy(MintedTokenAuctionTest, StandardToken.address,
-                                10000000000000000000000000,
-                                5000000000000000000000000,
-                                1000000000000000000,
-                                web3.toWei(10,'finney'),
-                                web3.toWei(1,'finney'),
-                                web3.toWei(1,'finney'),
-                                d);
-      });
-    }
-  } else {
-    deployer.link(BasicMathLib, MintedTokenAuction);
-    deployer.link(ArrayUtilsLib, MintedTokenAuction);
-    deployer.link(ERC20Lib, MintedTokenAuction);
-/*  Set constructor to desired parameters and uncomment when ready to go live
-    var d = new Date().valueOf();
-    d = (d+5000000)/1000;
-    deployer.deploy(MintedTokenAuctionTest, [Need token address], 
-                    10000000000000000000000000,
-                    5000000000000000000000000,
-                    1000000000000000000,
-                    web3.toWei(10,'finney'),
-                    web3.toWei(1,'finney'),
-                    web3.toWei(1,'finney'),
-                    d);
-*/
-  }
+  deployer.link(BasicMathLib, MintedTokenAuctionTest);
+  deployer.link(ArrayUtilsLib, MintedTokenAuctionTest);
+  deployer.link(ERC20Lib, MintedTokenAuctionTest);
+  deployer.link(ERC20Lib, StandardToken);
 };
--- StandardICOAuction/truffle/migrations/2_test_contracts.js
+++ StandardICOAuction/truffle/migrations/2_test_contracts.js
@@ -10,29 +10,7 @@ module.exports = function(deployer, network) {
   deployer.deploy(ArrayUtilsLib,{overwrite: false});
   deployer.link(BasicMathLib, ERC20Lib);
   deployer.deploy(ERC20Lib,{overwrite: false});
-  if(network !== "live"){
-    deployer.link(BasicMathLib, ICOAuctionTestContract);
-    deployer.link(ArrayUtilsLib, ICOAuctionTestContract);
-    deployer.link(ERC20Lib, ICOAuctionTestContract);
-    if(network == "rinkeby") {
-      var d = new Date().valueOf();
-      d = (d-5000)/1000;
-      deployer.deploy(ICOAuctionTestContract, web3.toWei(10000,'ether'),
-                      web3.toWei(1000,'ether'), web3.toWei(10,'ether'),
-                      web3.toWei(10,'finney'), web3.toWei(1,'finney'),
-                      web3.toWei(1,'finney'), 18, 90, d);
-    }
-  } else {
-    deployer.link(BasicMathLib, StandardICOAuction);
-    deployer.link(ArrayUtilsLib, StandardICOAuction);
-    deployer.link(ERC20Lib, StandardICOAuction);
-/*  Set constructor to desired parameters and uncomment when ready to go live
-    var d = new Date().valueOf();
-    d = (d+5000000)/1000;
-    deployer.deploy(StandardICOAuction, web3.toWei(10000,'ether'),
-                    web3.toWei(1000,'ether'), web3.toWei(10,'ether'),
-                    web3.toWei(10,'finney'), web3.toWei(1,'finney'),
-                    web3.toWei(1,'finney'), 18, 90, d);
-*/
-  }
+  deployer.link(BasicMathLib, ICOAuctionTestContract);
+  deployer.link(ArrayUtilsLib, ICOAuctionTestContract);
+  deployer.link(ERC20Lib, ICOAuctionTestContract);
 };
